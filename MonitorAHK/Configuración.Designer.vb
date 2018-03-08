769771a4ff9eb285591e15edc26a348b05ffb383
Daniel Medrano <bestenglishking@hotmail.com>
2018-03-07 19:13:01 -0600

    Modificar diseño

--- MonitorAHK/Configuración.Designer.vb
+++ MonitorAHK/Configuración.Designer.vb
@@ -22,16 +22,124 @@ Partial Class frmConfig
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtDatabaseServer = New System.Windows.Forms.TextBox()
+        Me.chkWindowsAuth = New System.Windows.Forms.CheckBox()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(8, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(131, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Servidor de base de datos"
+        '
+        'txtDatabaseServer
+        '
+        Me.txtDatabaseServer.Location = New System.Drawing.Point(160, 16)
+        Me.txtDatabaseServer.Name = "txtDatabaseServer"
+        Me.txtDatabaseServer.Size = New System.Drawing.Size(184, 20)
+        Me.txtDatabaseServer.TabIndex = 1
+        '
+        'chkWindowsAuth
+        '
+        Me.chkWindowsAuth.AutoSize = True
+        Me.chkWindowsAuth.Location = New System.Drawing.Point(16, 48)
+        Me.chkWindowsAuth.Name = "chkWindowsAuth"
+        Me.chkWindowsAuth.Size = New System.Drawing.Size(153, 17)
+        Me.chkWindowsAuth.TabIndex = 2
+        Me.chkWindowsAuth.Text = "Autenticación de Windows"
+        Me.chkWindowsAuth.UseVisualStyleBackColor = True
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(128, 80)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(184, 20)
+        Me.txtUsername.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(48, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Usuario"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(128, 104)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(184, 20)
+        Me.txtPassword.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(48, 104)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Contraseña"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(88, 136)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Guardar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(184, 136)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Cancelar"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'frmConfig
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(352, 168)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.chkWindowsAuth)
+        Me.Controls.Add(Me.txtDatabaseServer)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
         Me.Name = "frmConfig"
         Me.Text = "Configuración"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtDatabaseServer As TextBox
+    Friend WithEvents chkWindowsAuth As CheckBox
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
