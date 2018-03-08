ce73792d25d0fcfd3a5321076c80c552770fe7f0
Daniel Medrano <bestenglishking@hotmail.com>
2018-03-07 19:27:25 -0600

    Corrección de nombres de botones

--- MonitorAHK/Configuración.Designer.vb
+++ MonitorAHK/Configuración.Designer.vb
@@ -29,8 +29,8 @@ Partial Class frmConfig
         Me.Label2 = New System.Windows.Forms.Label()
         Me.txtPassword = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btSave = New System.Windows.Forms.Button()
+        Me.btCancel = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -91,31 +91,31 @@ Partial Class frmConfig
         Me.Label3.TabIndex = 5
         Me.Label3.Text = "Contraseña"
         '
-        'Button1
+        'btSave
         '
-        Me.Button1.Location = New System.Drawing.Point(88, 136)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 7
-        Me.Button1.Text = "Guardar"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btSave.Location = New System.Drawing.Point(88, 136)
+        Me.btSave.Name = "btSave"
+        Me.btSave.Size = New System.Drawing.Size(75, 23)
+        Me.btSave.TabIndex = 7
+        Me.btSave.Text = "Guardar"
+        Me.btSave.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btCancel
         '
-        Me.Button2.Location = New System.Drawing.Point(184, 136)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 8
-        Me.Button2.Text = "Cancelar"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btCancel.Location = New System.Drawing.Point(184, 136)
+        Me.btCancel.Name = "btCancel"
+        Me.btCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btCancel.TabIndex = 8
+        Me.btCancel.Text = "Cancelar"
+        Me.btCancel.UseVisualStyleBackColor = True
         '
         'frmConfig
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(352, 168)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btCancel)
+        Me.Controls.Add(Me.btSave)
         Me.Controls.Add(Me.txtPassword)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.txtUsername)
@@ -140,6 +140,6 @@ Partial Class frmConfig
     Friend WithEvents Label2 As Label
     Friend WithEvents txtPassword As TextBox
     Friend WithEvents Label3 As Label
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
+    Friend WithEvents btSave As Button
+    Friend WithEvents btCancel As Button
 End Class
